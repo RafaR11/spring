748b09d9e5b78f5ca227f820334467d205193769
Rafa <rafael.romero@institutvidreres.cat>
2022-01-20 22:35:18 +0100

    commit17

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
 public class HerokuApplication {
-    @GetMapping("/name")
-    public String name(){
-        return "name.html";
+    @RequestMapping(value="/")  //or Method="POST"
+    public String welcome() {
+        return "/name";
     }
     @ResponseBody
     public static void main(String[] args) {
