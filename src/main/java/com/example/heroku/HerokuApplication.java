779044488df8d0e6b90a2996c2ee399bbd2fee76
Rafa <rafael.romero@institutvidreres.cat>
2022-01-20 20:53:29 +0100

    commit6

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -3,6 +3,7 @@ package com.example.heroku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,8 +13,7 @@ import java.lang.reflect.Array;
 @Controller
 @SpringBootApplication
 public class HerokuApplication {
-    @RequestMapping("/")
-    @PostMapping("/imprimirArray")
+    @GetMapping("/imprimirArray")
     @ResponseBody
     String[] home(){
         String[] array = new String[] {"rojo", "verde", "amarillo"};
